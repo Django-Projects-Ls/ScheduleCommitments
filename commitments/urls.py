d16913d8456b22d272b1f1a8f688cb223342e812
LopesLs <lopes.carlos.host@gmail.com>
2023-10-23 09:19:17 -0300

    feature

--- commitments/urls.py
+++ commitments/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.commitments_request_handler, name='commitment'),
     path('details/<int:id>', views.details_request_handler, name='commitment details'),
-    path('create-forms', views.create_commitment, name='create commitment')   
+    path('create-forms', views.create_commitment, name='create commitment'),
+    path('details/edit-forms/<int:pk>', views.edit_commitment, name='edit commitment'),
+    path('details/delete-forms/<int:pk>', views.delete_commitment, name='delete commitment') 
 ]
--- commitments/views.py
+++ commitments/views.py
@@ -10,7 +10,7 @@ def details_request_handler(request, id):
 
 def create_commitment(request):
     if request.method == "POST":
-        form = CommitmentsForms(request.POST)
+        form = CommitmentsForms(request.POST) # Getting our params of our request
         
         if form.is_valid():
             form.save()
@@ -20,3 +20,24 @@ def create_commitment(request):
     elif request.method == "GET":
         form = CommitmentsForms()
         return render(request, 'form.html', {'form' : form})
+
+def edit_commitment(request, pk):
+    commitment = Commitment.objects.get(id=pk)
+    
+    if request.method == "POST":
+        form = CommitmentsForms(request.POST, instance=commitment)
+        
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')
+    
+    else:
+        form = CommitmentsForms(instance=commitment)
+        return render(request, 'form.html', {'form' : form})
+
+def delete_commitment(request, pk):
+    commitment = Commitment.objects.get(id=pk)
+    commitment.delete()
+    
+    return render(request, 'excluir.html')
